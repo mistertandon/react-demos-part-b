c83c06d2ae3d6682838854c6469f9116b4fe9575
mistertandon <enggparveshtandon@gmail.com>
2022-01-04 23:01:11 +0530

    feat: basic setup.

---
+++ a01_custom-select/src/components/CustomSelect.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./CustomSelect.css";
+
+const CustomSelect = () => {
+
+    return (<div>
+
+        Hello from CustomSelect
+    </div>)
+}
+
+export default CustomSelect;
