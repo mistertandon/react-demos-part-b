15be5b68b72cb65383842ea869b6f79371c489bd
mistertandon <enggparveshtandon@gmail.com>
2022-01-05 22:06:41 +0530

    feat: implement basic functionality of custom select

--- a01_custom-select/src/App.js
+++ a01_custom-select/src/App.js
@@ -1,13 +1,51 @@
+import { useState } from 'react';
 import './App.css';
 import CustomSelect from './components/CustomSelect';
 
 function App() {
+  const selectOptions = [
+    {
+      title: 'One',
+      value: 1,
+    },
+    {
+      title: 'Two',
+      value: 2,
+    },
+    {
+      title: 'Three',
+      value: 3,
+    },
+    {
+      title: 'Four',
+      value: 4,
+    },
+    {
+      title: 'Five',
+      value: 5,
+    }
+  ];
+
+  const defaultOption = { title: 'Select Option', value: '' };
+
+  const [selectFieldAgeError, setSelectFieldAgeError] = useState(false);
+
   return (
-    <div className="App">
+    <div className="app--container">
       <header className="App-header">
 
       </header>
-      <CustomSelect />
+      <div className='child-a--ac'>
+        <CustomSelect selectOptions={selectOptions}
+          defaultOption={defaultOption}
+          fieldErrorStatus={selectFieldAgeError}
+          setFieldErrorStatus={setSelectFieldAgeError}
+        />
+      </div>
+
+      <div>
+        Hello
+      </div>
 
     </div>
   );
--- a01_custom-select/src/components/CustomSelect.js
+++ a01_custom-select/src/components/CustomSelect.js
@@ -1,12 +1,103 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import "./CustomSelect.css";
 
-const CustomSelect = () => {
+const CustomSelect = ({ selectOptions, defaultOption, fieldErrorStatus, setFieldErrorStatus }) => {
 
-    return (<div>
+    const searchVal = useRef();
+    const [optionsContainerDisplayStatus, setOptionsContainerDisplayStatus] = useState(true);
+    const [optionContainerWidth, setOptionContainerWidth] = useState('auto');
+    // const [optionsContainerDisplayStatus, setOptionsContainerDisplayStatus] = useState(false);
 
-        Hello from CustomSelect
-    </div>)
+    const optionContainerRef = useRef(null);
+
+    useEffect(() => {
+
+        setTimeout(() => {
+            setOptionContainerWidth(`${optionContainerRef.current.offsetWidth}px`);
+        }, 0);
+    }, [selectOptions]);
+
+    const handleSearchVal = () => {
+        console.log('searchVal', searchVal.current.value);
+    }
+
+    const handleDisplayOptionsEvent = () => {
+        console.log('handleDisplayOptionsEvent');
+        setOptionsContainerDisplayStatus(true);
+    }
+
+    const handleHideOptionsEvent = () => {
+        console.log('handleHideOptionsEvent');
+        // setOptionsContainerDisplayStatus(false);
+    }
+
+    const handleOptionSelection = (optn) => {
+        console.log('handleOptionSelection', optn);
+        // fieldErrorStatus && setFieldErrorStatus(false);
+    }
+
+    const handleDefaultOptionSelection = () => {
+        console.log('handleDefaultOptionSelection');
+        setFieldErrorStatus(true);
+    }
+
+    return (
+        <div className={`custom-select--cont ${fieldErrorStatus === true ? 'select-error' : 'nope'}`}
+            style={{ width: optionContainerWidth }}
+        >
+
+            <div className='child-a--csc'>
+                <div className='ca-child-a--csc'
+                    tabIndex='1'
+                    onFocus={handleDisplayOptionsEvent}
+                    onBlur={handleHideOptionsEvent}
+                >
+                    <div className='caca-child-b--csc'>
+                        {defaultOption.title}
+                    </div>
+
+                    <span className='caca-child-a--csc'>
+
+                    </span>
+
+                </div>
+
+                <div className={`ca-child-b--csc ${optionsContainerDisplayStatus === true ? 'display-options' : 'hide-options'}`}
+                    ref={optionContainerRef}
+                >
+                    <div className='cacb-child-a--csc'>
+                        <input type='text'
+                            ref={searchVal}
+                            onChange={handleSearchVal}
+                            className='cacb-child-a--csc'
+                        />
+                    </div>
+                    <div className='cacb-child-b--csc'>
+                        <div className='cacbcb-child-a--csc option-child--scs default_option'
+                            key='default_option'
+                            onClick={() => {
+                                handleDefaultOptionSelection();
+                            }}
+                        >
+                            Select Option
+                        </div>
+                        {
+                            selectOptions.map(optn => (
+                                <div className='option-child--scs'
+                                    key={optn.value}
+                                    onClick={() => {
+                                        handleOptionSelection(optn);
+                                    }}>
+                                    {optn.value}
+                                </div>
+                            ))
+                        }
+                    </div>
+
+                </div>
+            </div>
+
+        </div>)
 }
 
 export default CustomSelect;
